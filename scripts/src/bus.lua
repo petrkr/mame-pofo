70244b44269131294765d026c2086fceb03e13c4
Petr Kracik <petrkr@petrkr.net>
2026-09-21 18:14:00 +0200

    Pofo: Added pofo sources to bus lua build

--- scripts/src/bus.lua
+++ scripts/src/bus.lua
@@ -4730,6 +4730,8 @@ if BUSES["POFO"] then
 		MAME_DIR .. "src/devices/bus/pofo/hpc102.h",
 		MAME_DIR .. "src/devices/bus/pofo/hpc104.cpp",
 		MAME_DIR .. "src/devices/bus/pofo/hpc104.h",
+		MAME_DIR .. "src/devices/bus/pofo/pofo_smartcable.cpp",
+		MAME_DIR .. "src/devices/bus/pofo/pofo_smartcable.h",
 		MAME_DIR .. "src/devices/bus/pofo/ccm.cpp",
 		MAME_DIR .. "src/devices/bus/pofo/ccm.h",
 		MAME_DIR .. "src/devices/bus/pofo/ram.cpp",
